cd56dc277311957cec379ee02407dbd8face2468
Jorge Rotter <jarotter@Jorges-MacBook-Pro.local>
2019-11-10 03:49:48 -0600

    hotfix [3-bayesopt]: changes inf for largest possible float

--- scooter-solver/laboratory.py
+++ scooter-solver/laboratory.py
@@ -2,26 +2,15 @@ from ax import RangeParameter, ParameterType, ParameterConstraint, SearchSpace
 import numpy as np
 from contest.utility_functions import IndependentUtilityFunction
 
-class ExperimentBuilder():
-    def __init__(self, 
-        N, 
-        name, 
-        mc_samples = 100,
-        tfunc = "E[u]-indv1",
-        total_trials = 30,
-    ):
+class SearchSpaceFactory():
+    def __init__(self, N):
         self.N = N
-        self.name = name
-        self.objective_name = tfunc,
-        self.minimize = False,
-        self.total_trials = total_trials
-        self.mc_samples = mc_samples
-        
+
     def u(self, i):
         return RangeParameter(
             name = f"u{i}",
             parameter_type = ParameterType.INT,
-            lower = 1,
+            lower = 1, 
             upper = 35
         )
     
@@ -29,15 +18,15 @@ class ExperimentBuilder():
         return RangeParameter(
                 name = f"c{i}",
                 parameter_type = ParameterType.FLOAT,
-                lower = 1 + 1e-6,
-                upper = np.inf
+                lower = 1+1e-6, 
+                upper = np.finfo(float).max
         )
     
     def D(self, i, j):
         return RangeParameter(
-            name = f"D{i},{j}]",
+            name = f"D({i},{j})",
             parameter_type = ParameterType.INT,
-            lower = 0,
+            lower = 0, 
             upper = 1
         )
     
@@ -45,8 +34,8 @@ class ExperimentBuilder():
         u = [self.u(i) for i in range(self.N)]
         c = [self.c(i) for i in range(self.N)]
         D = []
-        for i in range(self.N):
-            for j in range(i):
+        for j in range(self.N):
+            for i in range(j):
                 D.append(self.D(i, j))
                 
         return u + c + D
@@ -55,31 +44,31 @@ class ExperimentBuilder():
         M = self.N + 1
         c1 = ParameterConstraint(
             constraint_dict = {
-                f"u{i}": 1.0,
-                f"u{j}": -1.0,
-                f"D{i},{j}" : -M
+                f"u{i}" : 1,
+                f"u{j}" : -1,
+                f"D({i},{j})" : -M
             },
-            bound = -1.0
+            bound = -1
         )
         c2 = ParameterConstraint(
             constraint_dict = {
-                f"u{i}": -1.0,
-                f"u{j}": 1.0,
-                f"D{i},{j}": M
+                f"u{i}" : -1,
+                f"u{j}" : 1,
+                f"D({i},{j})" : M
             },
-            bound = -M
+            bound = M-1
         )
         return [c1, c2]
 
     def build_constraints(self):
         cons = []
-        for i in range(self.N):
-            for j in range(i):
+        for j in range(self.N):
+            for i in range(j):
                 cons += self.neq(i, j)
         return cons
 
     def build(self):
         return SearchSpace(
             parameters = self.build_parameters(),
-            parameter_constraints= self.build_constraints()
+            parameter_constraints = self.build_constraints()
         )
