c967a352954042030b2d8c6e21a2a30d30f1335a
Jorge Rotter <jarotter@Jorges-MacBook-Pro.local>
2020-04-21 23:35:22 -0500

    Chore: Adds docstring check to flake8

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,4 @@
+"""Nox sessions."""
 from tempfile import NamedTemporaryFile
 
 import nox
@@ -7,6 +8,7 @@ LOCATIONS = "src", "tests", "noxfile.py"
 
 
 def install_with_constraints(session: Session, *args: str) -> None:
+    """Install packages to the version specified by poetry."""
     with NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -21,6 +23,7 @@ def install_with_constraints(session: Session, *args: str) -> None:
 
 @nox.session(python=["3.8"])
 def tests(session: Session) -> None:
+    """Run test suite."""
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
     session.run("pytest", "--cov")
@@ -28,8 +31,22 @@ def tests(session: Session) -> None:
 
 @nox.session(python=["3.8"])
 def lint(session: Session) -> None:
+    """Lint using flake8 and plugins."""
     args = session.posargs or LOCATIONS
     install_with_constraints(
-        session, "flake8", "flake8-black", "flake8-isort", "flake8-annotations",
+        session,
+        "flake8",
+        "flake8-black",
+        "flake8-isort",
+        "flake8-annotations",
+        "flake8-docstrings",
     )
     session.run("flake8", *args)
+
+
+@nox.session(python=["3.8"])
+def mypy(session: Session) -> None:
+    """Type check using mypy."""
+    args = session.posargs or LOCATIONS
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
